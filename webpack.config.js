6e6c7ee1222959f933911a8e08fe96dab472ba39
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-10-28 13:47:07 -0700

    finished initial setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const appDirectory = fs.realpathSync(process.cwd());
 
 module.exports = {
